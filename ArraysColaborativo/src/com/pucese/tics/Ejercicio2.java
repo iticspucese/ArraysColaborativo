9b554b36a071e1e33603800f3c047a449b6ced6a
kevingo710 <kevin.gracia.tic@gmail.com>
2019-12-09 20:02:05 -0500

    Add Codigo link

--- ArraysColaborativo/src/com/pucese/tics/Ejercicio2.java
+++ ArraysColaborativo/src/com/pucese/tics/Ejercicio2.java
@@ -2,4 +2,10 @@ package com.pucese.tics;
 
 public class Ejercicio2 {
 
+	    public static void main(String[] args) { 
+	        int n = 1000;
+	        int[] a = new int[n*n*n*n];
+	        System.out.println(a.length);
+	    }
+
 }
